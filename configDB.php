5d2284812899912851a5ad502d86605f0a9877ca
Jeffrey <delvaljeffrey@gmail.com>
2021-04-13 02:19:26 -0500

    Registrar Bonsai

---
+++ configDB.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Database config variables
+ */
+define('DB_SERVER', 'localhost');
+define("DB_USERNAME", "root");
+define("DB_PASSWORD","");
+define("DB_DATABASE", "bonsais");
+
+
+$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+ 
+// Check connection
+if($mysqli === false){
+    die("ERROR: Could not connect. " . $mysqli->connect_error);
+}
+?>
+
