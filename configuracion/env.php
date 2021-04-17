1c82f002fe77a09f3f1f530efe6501f08c957959
Nadia Bravo <nait0197@live.com.mx>
2021-04-17 02:58:34 -0500

    Nueva variable
    Se agrega la nueva variable de entorno para los modelos

--- configuracion/env.php
+++ configuracion/env.php
@@ -3,6 +3,7 @@ define("URL_CSS", "../../publico/css/");
 define("URL_JS", "../../publico/js/");
 define("URL_IMAGENES", "../../publico/imagenes/");
 define("URL_CONTROLADORES", "../controlador/");
+define("URL_MODELOS", "../modelo/");
 define('DB_SERVER', 'localhost');
 define("DB_USERNAME", "root");
 define("DB_PASSWORD","");
