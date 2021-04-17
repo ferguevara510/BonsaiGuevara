9a848f0033df9b33e52346d4dbc19a371a863c31
Nadia Bravo <nait0197@live.com.mx>
2021-04-17 02:46:03 -0500

    Organización de carpetas de proyecto
    Se creo la estructura para todo el proyecto
    Se genero la plantilla para todas las vistas HTML
    Se genero la vista del CU-005 registrar cliente 
    Se creo la carpeta publico a donde se metieron todos los css, js e imagenes
    Se creo el archivo env donde se crearon la variables de entorno

---
+++ configuracion/env.php
@@ -0,0 +1,9 @@
+<?php
+define("URL_CSS", "../../publico/css/");
+define("URL_JS", "../../publico/js/");
+define("URL_IMAGENES", "../../publico/imagenes/");
+define("URL_CONTROLADORES", "../controlador/");
+define('DB_SERVER', 'localhost');
+define("DB_USERNAME", "root");
+define("DB_PASSWORD","");
+define("DB_DATABASE", "bonsaiguevara");
