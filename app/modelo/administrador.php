f0fc7e5b6dad5ed09f4052b21e05d625eb83c2c1
Nadia Bravo <nait0197@live.com.mx>
2021-04-24 01:24:11 -0500

    Consultar empresa
    Implementación del CU-016 Consultar empresa

--- app/modelo/administrador.php
+++ app/modelo/administrador.php
@@ -19,4 +19,26 @@ class Administrador {
         $this->numTelefono = '';
         $this->contrasena = '';
     }
+
+    public static function consultarEmpresa(){
+        $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+        $administrador = null;
+        $sql = "SELECT * FROM administrador";
+        if($result = $mysqli->query($sql)){
+            if($result->num_rows > 0){ 
+                if($row = $result->fetch_array()){
+                    $administrador = new Administrador();
+                    $administrador->nombre=$row["nombre"];
+                    $administrador->apellidoPaterno=$row["apellidoPaterno"];
+                    $administrador->apellidoMaterno=$row["apellidoMaterno"];
+                    $administrador->correo=$row["correo"];
+                    $administrador->numTelefono=$row["numTelefono"];
+                    $administrador->direccion=$row["direccion"];
+                }   
+            }
+            $result->close();
+        }
+        $mysqli->close();
+        return $administrador;
+    }
 }
