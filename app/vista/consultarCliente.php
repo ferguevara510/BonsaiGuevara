3dcd106a2c1524f545c90adc8e93e10fc00147c9
Nadia Bravo <nait0197@live.com.mx>
2021-04-29 21:13:05 -0500

    Cambios del enrutamiento
    Se comenzo navegabilidad del sistema

--- app/vista/consultarCliente.php
+++ app/vista/consultarCliente.php
@@ -47,88 +47,9 @@ $clientes = Cliente::buscarTodosClientes();
 </head>
 
 <body data-home-page="Iniciar-Sesión.html" data-home-page-title="Iniciar Sesión" class="u-body">
-    <header class="u-clearfix u-header u-header" id="sec-e89e">
-        <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
-            <a href="index.html" class="u-image u-logo u-image-1" data-image-width="299" data-image-height="266">
-            <img src="<?php echo URL_IMAGENES?>bonsai_karla.png" class="u-logo-image u-logo-image-1" data-image-width="64">
-            </a>
-            <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
-                <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
-                    <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base"
-                        href="#">
-                        <svg>
-                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use>
-                        </svg>
-                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg"
-                            xmlns:xlink="http://www.w3.org/1999/xlink">
-                            <defs>
-                                <symbol id="menu-hamburger" viewBox="0 0 16 16" style="width: 16px; height: 16px;">
-                                    <rect y="1" width="16" height="2"></rect>
-                                    <rect y="7" width="16" height="2"></rect>
-                                    <rect y="13" width="16" height="2"></rect>
-                                </symbol>
-                            </defs>
-                        </svg>
-                    </a>
-                </div>
-                <div class="u-custom-menu u-nav-container">
-                    <ul class="u-nav u-unstyled u-nav-1">
-                        <li class="u-nav-item"><a
-                                class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base"
-                                href="Iniciar-Sesión.html" style="padding: 10px 20px;">Pedidos</a></li>
-                        <li class="u-nav-item"><a
-                                class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base"
-                                href="Iniciar-Sesión.html" style="padding: 10px 20px;">Compras</a></li>
-                        <li class="u-nav-item"><a
-                                class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base"
-                                href="Iniciar-Sesión.html" style="padding: 10px 20px;">Cuidados</a></li>
-                        <li class="u-nav-item"><a
-                                class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base"
-                                href="Iniciar-Sesión.html" style="padding: 10px 20px;">Dudas</a></li>
-                        <li class="u-nav-item"><a
-                                class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base"
-                                href="Iniciar-Sesión.html" style="padding: 10px 20px;">Citas</a></li>
-                        <li class="u-nav-item"><a
-                                class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base"
-                                href="Iniciar-Sesión.html" style="padding: 10px 20px;">Empresa</a></li>
-                        <li class="u-nav-item"><a
-                                class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base"
-                                href="Iniciar-Sesión.html" style="padding: 10px 20px;">Cliente</a></li>
-                        <li class="u-nav-item"><a
-                                class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base"
-                                href="Iniciar-Sesión.html" style="padding: 10px 20px;">Iniciar Sesión</a></li>
-
-                    </ul>
-                </div>
-                <div class="u-custom-menu u-nav-container-collapse">
-                    <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
-                        <div class="u-sidenav-overflow">
-                            <div class="u-menu-close"></div>
-                            <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2">
-                                <li class="u-nav-item"><a class="u-button-style u-nav-link" href="Iniciar-Sesión.html"
-                                        style="padding: 10px 20px;">Pedidos</a></li>
-                                <li class="u-nav-item"><a class="u-button-style u-nav-link" href="Iniciar-Sesión.html"
-                                        style="padding: 10px 20px;">Compras</a></li>
-                                <li class="u-nav-item"><a class="u-button-style u-nav-link" href="Iniciar-Sesión.html"
-                                        style="padding: 10px 20px;">Cuidados</a></li>
-                                <li class="u-nav-item"><a class="u-button-style u-nav-link" href="Iniciar-Sesión.html"
-                                        style="padding: 10px 20px;">Dudas</a></li>
-                                <li class="u-nav-item"><a class="u-button-style u-nav-link" href="Iniciar-Sesión.html"
-                                        style="padding: 10px 20px;">Citas</a></li>
-                                <li class="u-nav-item"><a class="u-button-style u-nav-link" href="Iniciar-Sesión.html"
-                                        style="padding: 10px 20px;">Empresa</a></li>
-                                <li class="u-nav-item"><a class="u-button-style u-nav-link" href="Iniciar-Sesión.html"
-                                        style="padding: 10px 20px;">Cliente</a></li>
-                                <li class="u-nav-item"><a class="u-button-style u-nav-link" href="Iniciar-Sesión.html"
-                                        style="padding: 10px 20px;">Iniciar Sesión</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
-                </div>
-            </nav>
-        </div>
-    </header>
+    <?php
+    require_once "plantilla/menuAdmin.php";
+    ?>
     
     <h1 class="u-text u-text-default u-text-cliente .u-title">Lista de clientes</h1>
     <div class="contenedor-tarjetas">
--- configuracion/env.php
+++ configuracion/env.php
@@ -10,6 +10,9 @@ define("PATH_IMAGENES_PERFIL","../../publico/perfiles/");
 define("PATH_IMAGENES_BONSAIS","../../publico/bonsais/");
 define("URL_CSS_CALENDARIO", "../../publico/calendario/main.css");
 define("URL_JS_CALENDARIO", "../../publico/calendario/main.js");
+define("URL_CERRAR_SESION", "app/controlador/logout.php");
+define("URL_PLANTILLA", "plantilla/");
+define("PATH_VISTA", "app/vista/");
 
 
 define('DB_SERVER', 'localhost');
